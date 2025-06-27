2799e6c803c3373c6ac2f1384c6005260b87e725
Christopher Akaninyene Okoro <uraniumkid30@gmail.com>
2025-06-27 15:18:45 +0100

    Add files via upload

---
+++ test_monty/client.py
@@ -0,0 +1,87 @@
+from enum import StrEnum
+from typing import Any, Optional
+
+import httpx
+from loguru import logger
+
+from port_ocean.utils import http_async_client
+
+
+class ResourceCategory(StrEnum):
+    Event = "event/find"
+
+
+class SplunkClient:
+    def __init__(
+        self,
+        xsf_token: str,
+        ignore_server_error: bool,
+        allow_insecure: bool,
+        server_url: str = None,
+    ):
+        self.token = xsf_token
+        server_url = (
+            "https://api.eu1.signalfx.com" if server_url is None else server_url
+        )
+        self.api_url = f"{server_url}/v2"
+        self.ignore_server_error = ignore_server_error
+        self.allow_insecure = allow_insecure
+        self.api_auth_header = {"X-SF-Token": f"{self.token}"}
+        if self.allow_insecure:
+            # This is not recommended for production use
+            logger.warning(
+                "Insecure mode is enabled. This will disable SSL verification \
+                for the Splunk API client, which is not recommended for production use."
+            )
+            self.http_client = httpx.AsyncClient(verify=False)
+        else:
+            self.http_client = http_async_client
+        self.http_client.headers.update(self.api_auth_header)
+
+    async def _send_api_request(
+        self,
+        url: str,
+        method: str = "GET",
+        query_params: Optional[dict[str, Any]] = None,
+        json_data: Optional[dict[str, Any]] = None,
+    ) -> dict[str, Any]:
+        logger.info(f"Sending request to Splunk API: {method} {url}")
+        try:
+            async with self._semaphore:
+                response = await self.http_client.request(
+                    method=method,
+                    url=url,
+                    params=query_params,
+                    json=json_data,
+                )
+                response.raise_for_status()
+                return response.json()
+
+        except httpx.HTTPStatusError as e:
+            logger.error(
+                f"Encountered an HTTP error with status code: \
+                    {e.response.status_code} and response text: {e.response.text}"
+            )
+            if self.ignore_server_error:
+                return {}
+            raise e
+        except httpx.HTTPError as e:
+            logger.error(
+                f"Encountered an HTTP error {e} while sending a request to {method} {url} with query_params: {query_params}"
+            )
+            if self.ignore_server_error:
+                return {}
+            raise e
+
+    async def get_events(
+        self, query_params: Optional[dict[str, Any]]
+    ) -> list[dict[str, Any]]:
+        url = f"{self.api_url}/{ResourceCategory.event}"
+        try:
+            response_data = await self._send_api_request(url=url)
+            return response_data or []
+        except Exception as e:
+            logger.error(f"Failed to fetch resources of {ResourceCategory.event}: {e}")
+            if self.ignore_server_error:
+                return []
+            raise e
---
+++ test_monty/main.py
@@ -0,0 +1,92 @@
+from typing import Any
+
+from port_ocean.context.ocean import ocean
+from client import SplunkClient, ResourceCategory
+
+
+# Required
+# Listen to the resync event of all the kinds specified in the mapping inside port.
+# Called each time with a different kind that should be returned from the source system.
+@ocean.on_resync()
+async def on_resync(kind: str) -> list[dict[Any, Any]]:
+    # 1. Get all data from the source system
+    # 2. Return a list of dictionaries with the raw data of the state to run the core logic of the framework for
+    # Example:
+    # if kind == "project":
+    #     return [{"some_project_key": "someProjectValue", ...}]
+    # if kind == "issues":
+    #     return [{"some_issue_key": "someIssueValue", ...}]
+
+    # Initial stub to show complete flow, replace this with your own logic
+    if kind == "test_monty-example-kind":
+        return [
+            {
+                "id": "AddBYZrEFEF",
+                "metadata": {
+                    "ETS_key1": "detector",
+                    "ETS_key2": False,
+                    "ETS_key3": 1001,
+                },
+                "properties": {
+                    "is": "ok",
+                    "sf_notificationWasSent": True,
+                    "was": "anomalous",
+                },
+                "sf_eventCategory": "USER_DEFINED",
+                "sf_eventCreatedOnMs": 1553678621002,
+                "sf_eventType": "string",
+                "timestamp": 1554672630000,
+                "tsId": "XzZYApXCDCD",
+            }
+        ]
+
+    return []
+
+
+def init_client() -> SplunkClient:
+    return SplunkClient(
+        ocean.integration_config["token"],
+        ocean.integration_config["server_url"],
+        ocean.integration_config["ignore_server_error"],
+        ocean.integration_config["allow_insecure"],
+    )
+
+
+@ocean.on_resync(kind=ResourceCategory.Event)
+async def on_resources_resync(kind: str):
+    argocd_client = init_client()
+    return await argocd_client.get_events({"query": "is:ok"})
+
+
+# The same sync logic can be registered for one of the kinds that are available in the mapping in port.
+# @ocean.on_resync('project')
+# async def resync_project(kind: str) -> list[dict[Any, Any]]:
+#     # 1. Get all projects from the source system
+#     # 2. Return a list of dictionaries with the raw data of the state
+#     return [{"some_project_key": "someProjectValue", ...}]
+#
+# @ocean.on_resync('issues')
+# async def resync_issues(kind: str) -> list[dict[Any, Any]]:
+#     # 1. Get all issues from the source system
+#     # 2. Return a list of dictionaries with the raw data of the state
+#     return [{"some_issue_key": "someIssueValue", ...}]
+
+
+# Optional
+# Listen to the start event of the integration. Called once when the integration starts.
+@ocean.on_start()
+async def on_start() -> None:
+    # Something to do when the integration starts
+    # For example create a client to query 3rd party services - GitHub, Jira, etc...
+    print("Starting test_monty integration")
+
+
+@ocean.router.post("/webhook")
+async def on_application_event_webhook_handler(request) -> None:
+    data = await request.json()
+    print(f"received webhook event data: {data}")
+    splunk_client = init_client()
+
+    if data["action"] == "upsert":
+        application = await splunk_client.get_events({"query": "is:ok"})
+        await ocean.register_raw(ResourceCategory.Event, [application])
